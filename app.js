995234bfa4d804bec4ff5768aeba5294ee581eae
Carlos Souza <carloshrsouza@gmail.com>
2014-02-23 15:50:56 -0500

    First stab at facebook auth.

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ var event = require('./routes/event');
 var http = require('http');
 var path = require('path');
 
+var passport = require('passport'),
+    FacebookStrategy = require('passport-facebook').Strategy;
+
 var app = express();
 
 // all environments
@@ -20,6 +23,11 @@ app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
+app.use(express.cookieParser());
+app.use(express.bodyParser());
+app.use(express.session({ secret: 'bananas' }));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -28,8 +36,36 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+passport.use(new FacebookStrategy({
+    clientID: process.env.FB_ID,
+    clientSecret: process.env.FB_SECRET,
+    callbackURL: (process.env.URL || 'http://localhost:3000') +
+      '/auth/facebook/callback'
+  },
+  function(accessToken, refreshToken, profile, done) {
+    done(null, profile);
+  }
+));
+
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+  done(null, user);
+});
+
 app.get('/', event.upcoming);
 app.get('/upcomingFights', event.upcomingFights);
+app.get('/auth/facebook', passport.authenticate('facebook'));
+app.get('/auth/facebook/callback', passport.authenticate('facebook', {
+  successRedirect: '/',
+  failureRedirect: '/failed'
+}))
+app.get('/failed', function (req, res) {
+  res.send(401, 'Error Authenticating on Facebook')
+});
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/event.js
+++ routes/event.js
@@ -3,30 +3,19 @@
  * returns upcoming event
  */
 exports.upcoming = function(req, res){
-  // TODO: replace hardcoded with something like the following:
-  // var event = Event.findUpcoming();
-  var event = { title: 'UFC 170', fights: [] };
+  var event = { title: 'UFC 170' },
+      userName = null;
 
-  event.fights.push({
-    id: 1,
-    division: 'Bantamweight',
-    fighters: [ 'Ronda Rousey', 'Sara McMann' ]
-  });
-  event.fights.push({
-    id: 2,
-    division: 'Heavyweight',
-    fighters: [ 'Daniel Cormier', 'Patrick Cummins' ]
-  });
-  event.fights.push({
-    id: 3,
-    division: 'Welterweight',
-    fighters: [ 'Rory McDonald', 'Demian Maia' ]
-  });
+  if(req.isAuthenticated()){
+    userName = req.user.displayName;
+  }
 
-  res.render('upcoming', { event: event });
+  res.render('upcoming', { event: event, userName: userName });
 }
 
 exports.upcomingFights = function (req, res) {
+  // TODO: replace hardcoded with something like the following:
+  // var event = Event.findUpcoming();
   var fights = [];
   fights.push({
     id: 1,
